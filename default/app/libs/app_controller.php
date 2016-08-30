8a45629e27a883c1e623c6e0f4f2984cf49af9bb
SergioDorado <sad_35842@hotmail.com>
2016-08-30 12:03:12 -0300

    navbar
    Incorporacion de barra de navegacion

--- default/app/libs/app_controller.php
+++ default/app/libs/app_controller.php
@@ -19,7 +19,7 @@ class AppController extends Controller
 
     final protected function initialize()
     {
-
+        View::template('MasterPage');
     }
 
     final protected function finalize()
