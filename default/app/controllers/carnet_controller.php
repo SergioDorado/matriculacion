413c0e53a8ab8876742c9881b385187f039143b2
SergioDorado <sad_35842@hotmail.com>
2016-09-01 11:16:54 -0300

    01/09/2016
    Se acomodaron los datos en el frente del carnet

--- default/app/controllers/carnet_controller.php
+++ default/app/controllers/carnet_controller.php
@@ -5,6 +5,15 @@ class CarnetController extends AppController
     public function GenerarCarnet()
     {
         View::template(NULL);
-        $this->NomCar = 'hola mundo';
+        $this->NomCar = 'NOMBRE1 NOMBRE2';
+        $this->ApeCar = 'APELLIDO';
+        $this->DniCar = '12345678';
+        $this->NacCar = 'ARGENTINA';
+        $this->SexoCar = 'M';
+        $this->FecEmiCar = '15/07/2015';
+        $this->FecVenCar = '15/07/2020';
+        $this->CodProfCar ='12345678912345';
+        $this->ProfCar = 'TECNICO RADIOLOGO';
+        $this->NumMatCar ='U026304M';
     }
 }
